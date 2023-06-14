65734680050eb1965d830993d97702c06f9498e2
ainul-hossain <ainulsakibs@gmail.com>
2023-06-14 22:45:18 +0600

    users, posts route done!

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-const authRoute = require('./routes/auth.js');
+const authRoute = require('./routes/auth.js');  // for register & login
+const usersRoute = require('./routes/users.js');
+const postsRoute = require('./routes/posts.js');
 
 dotenv.config();
 app.use(express.json());
@@ -13,6 +15,8 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err)=>console.log(err));
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/posts', postsRoute);
 
 app.listen('5000', () => {
     console.log('Backend is running...');
