f09815db40cb85f0e9eb61fa283cb8efd98d4401
ainul-hossain <ainulsakibs@gmail.com>
2023-06-15 00:24:58 +0600

    all routes & file upload with multer done!

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const authRoute = require('./routes/auth.js');  // for register & login
 const usersRoute = require('./routes/users.js');
 const postsRoute = require('./routes/posts.js');
+const categoriesRoute = require('./routes/categories.js');
 
 dotenv.config();
 app.use(express.json());
@@ -14,9 +16,25 @@ mongoose.connect(process.env.MONGO_URL)
     .then(console.log('connected'))
     .catch((err)=>console.log(err));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'images');
+    }, 
+
+    filename: (req, file, cb)=>{
+        cb(null, 'hello.jpg');
+    }
+});
+
+const upload = multer({storage: storage});
+app.post('/api/upload', upload.single('file'), (req, res)=>{
+    res.status(200).json('File has been uploaded!');
+})
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/posts', postsRoute);
+app.use('/api/categories', categoriesRoute);
 
 app.listen('5000', () => {
     console.log('Backend is running...');
